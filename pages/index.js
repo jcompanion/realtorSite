eac0e3eaa536566816171dddb6b199446f07b273
Josh Companion <31940636+jcompanion@users.noreply.github.com>
2022-02-13 09:59:28 -0600

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="items-center flex w-full justify-center h-screen p-4">
+      <h1 className="transition-all text-3xl p-4 bg-gray-200 rounded shadow-lg hover:scale-105 hover:shadow-3xl hover:bg-green-100">
+        Hello, code time bb. GET STARTED!
+      </h1>
+    </div>
+  );
+}
